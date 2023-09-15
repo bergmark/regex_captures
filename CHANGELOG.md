967962bef62679c47ce3b252d6f0f44bf033c8c3
Adam Bergmark <adam@bergmark.nl>
2023-09-15 12:15:37 +0200

    feat: Add Captures::parse_name

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## 0.1.1
+
+Add `Captures::parse_name` for the `FromStr` + named capture combination
+
+# 0.1.0
+
+Initial release
--- src/lib.rs
+++ src/lib.rs
@@ -59,6 +59,22 @@ impl<'a> Captures<'a> {
             .map(|m| m.as_str().into())
     }
 
+    pub fn parse_name<A>(&self, name: &'static str) -> Result<A, anyhow::Error>
+    where
+        A: FromStr,
+        A::Err: Debug,
+    {
+        let value = self
+            .0
+            .name(name)
+            .with_context(|| format!("Could not find capture group {name}"))?
+            .as_str();
+
+        value
+            .parse()
+            .map_err(|e| anyhow!("Could not parse {value} in capture group {name}, error: {e:?}"))
+    }
+
     pub fn try_name<A>(&self, name: &'static str) -> Result<A, anyhow::Error>
     where
         A: TryFrom<&'a str>,
